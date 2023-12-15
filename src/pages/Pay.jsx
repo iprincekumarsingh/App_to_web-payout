d32db54ebcca0a3e628ed2ff5d588c9adc8bd3ca
Prince <princekumar2000.pks@gmail.comcllear>
2023-12-16 02:52:41 +0530

    Added pay amount

--- src/pages/Pay.jsx
+++ src/pages/Pay.jsx
@@ -12,7 +12,7 @@ const Pay = () => {
   const tokenParam = params.get("token") || "default_token";
   const orderIdParam = params.get("amount") || "default_order_id";
 
-  const buy = async () => {
+  const fetchDataAndBuy = async () => {
     try {
       const response = await axios.post(
         "https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-token",
@@ -20,7 +20,7 @@ const Pay = () => {
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${tokenParam}`, // Replace with your actual API key
+            Authorization: `Bearer ${tokenParam}`,
           },
         }
       );
@@ -28,29 +28,39 @@ const Pay = () => {
       setToken(response.data.data.orderToken);
 
       if (response.data.data.orderToken !== null) {
-        const orderResponse = await axios.post(
-          "https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-order",
-          {
-            amount: order_id,
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${tokenParam}`, // Replace with your actual API key
+        const orderResponse = await axios
+          .post(
+            "https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-order",
+            {
+              amount: 400,
             },
-          }
-        );
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${tokenParam}`,
+              },
+            }
+          )
+          .then((res) => {
+            console.log(res);
+            setOrder_id(res.data.order_id);
 
-        setOrder_id(orderResponse.data.order_id);
+            const options = {
+              access_key: "access_key_81x7BagWlM1Rn05N",
+              order_id: res.data.order_id,
+              callback_handler: function (response) {
+                if (response.status === "success") {
+                  alert("Payment successful");
+                }
+              },
+            };
 
-        const options = {
-          access_key: "access_key_81x7BagWlM1Rn05N", // Replace with your actual access key
-          order_id: orderResponse.data.order_id,
-          // ... other options
-        };
-
-        window.checkout = new NimbblCheckout(options);
-        window.checkout.open(orderResponse.data.order_id);
+            window.checkout = new NimbblCheckout(options);
+            window.checkout.open(res.data.order_id);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       }
     } catch (error) {
       console.error("Error:", error.message);
@@ -69,18 +79,19 @@ const Pay = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (tokenParam !== "default_token" && orderIdParam !== "default_order_id") {
+      fetchDataAndBuy();
+    }
+  }, []);
+
   return (
     <div className="App">
       <Helmet>
-        <script
-          src="https://api.nimbbl.tech/static/assets/js/checkout.js"
-          async
-        />
+        <script src="https://api.nimbbl.tech/static/assets/js/checkout.js" async />
       </Helmet>
       <header className="App-header">
-        <h1 onClick={buy}>Buy</h1>
-        <p>Token: {token || tokenParam}</p>
-        <p>Order ID: {order_id || orderIdParam}</p>
+        <h1>Click here to pay out</h1>
       </header>
     </div>
   );
