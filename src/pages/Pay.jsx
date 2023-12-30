1f2dda608d8f58a1271ecfd9f7b67f936dd54cf2
Prince <princekumar2000.pks@gmail.com>
2023-12-30 21:05:21 +0530

    Added the fix| new Interface

--- src/pages/Pay.jsx
+++ src/pages/Pay.jsx
@@ -1,36 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Helmet } from "react-helmet";
 import { useSearchParams, useNavigate } from "react-router-dom";
 
-const Pay = () => {
+const UpgradePage = () => {
   const [token, setToken] = useState("");
-
+  const [orderStatus, setOrderStatus] = useState("");
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const tokenParam = searchParams.get("token") || "default_token";
   const planIdParam = searchParams.get("planId") || "default_order_id";
-  const plan_type = searchParams.get("plan_type") || "one_time"
+  const plan_type = searchParams.get("plan_type") || "one_time";
+  const role = searchParams.get("role") || "default_role";
+  const isupgrade = searchParams.get("isupgrade") || "false";
+  const [loading, setLoading] = useState(false);
 
   const fetchDataAndBuy = async () => {
     try {
       if (
         tokenParam === "default_token" ||
-        planIdParam === "default_order_id"
+        planIdParam === "default_order_id" ||
+        role === "default_role"
       ) {
-        alert("Please select a plan first");
-        return;
-      }
-      if (tokenParam === null || planIdParam === null) {
-        alert("Please select a plan first");
+        console.log(tokenParam, planIdParam, role, isupgrade);
         return;
       }
 
+      // creating token for
       const response = await axios.post(
         "https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-token",
         {
           plan_id: planIdParam,
-          plan_type:plan_type
+          plan_type: plan_type,
         },
         {
           headers: {
@@ -44,7 +45,7 @@ const Pay = () => {
 
       if (response.data.data.orderToken !== null) {
         const orderResponse = await axios.post(
-          "https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-order",
+          `https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-order?isupgrade=true&role=${role}`,
           {
             plan_id: planIdParam,
           },
@@ -58,50 +59,87 @@ const Pay = () => {
 
         console.log(orderResponse.data.data);
 
+        if (orderResponse.data.data.paymentInitiated) {
+          // Check if payment has already been initiated
+          setLoading(false);
+          return;
+        }
+
         const options = {
           access_key: "access_key_81x7BagWlM1Rn05N",
           order_id: orderResponse.data.data.order_id,
-          callback_handler: function (response) {
-            console.log(response);
+          callback_handler: async function (response) {
+            // set order status
+            setOrderStatus(response.status);
+            if (response.status === "failed") {
+              // alert("Payment failed");
+
+              // Make the API request even for failed payments
+              await sendOrderAndCreatePayment(response);
+            }
+
             if (response.status === "success") {
               console.log(response.nimbbl_transaction_id);
               console.log(response.order_id);
               console.log(response.order.invoice_id);
               console.log(response.transaction.transaction_amount);
 
-              axios
-                .post(
-                  "https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-payment",
-                  {
-                    nimbbl_order_id: response.order_id,
-                    nimbbl_transaction_id: response.nimbbl_transaction_id,
-                    order_invoice: response.order.invoice_id,
-                    order_amount: response.transaction.transaction_amount,
-                    transaction_id: response.transaction.transaction_id,
-                    order_status: response.status,
-                    plan_type:plan_type
-                  },
-                  {
-                    headers: {
-                      "Content-Type": "application/json",
-                      Authorization: `Bearer ${tokenParam}`,
-                    },
-                  }
-                )
-                .then((res) => {
-                  console.log(res);
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
+              // Make the API request for successful payments
+              await sendOrderAndCreatePayment(response);
 
-                // change the url to success page
-              window.location.href = "/pay/success";
-           
-            } else if (response.status === "failure") {
-              alert("Payment failed");
+              // Additional logic for successful payments
             }
           },
+        };
+
+        // Function to send order status and create detailed payment
+        const sendOrderAndCreatePayment = async (response) => {
+          try {
+            // Your logic to update order status
+
+            // Call the createPayment function
+            await createPayment(response);
+          } catch (error) {
+            console.error("Error:", error.message);
+          }
+        };
+
+        // Function to create detailed payment
+        // Function to create detailed payment
+        const createPayment = async (response) => {
+          try {
+            let payload = {
+              nimbbl_order_id: response.order_id || "",
+              nimbbl_transaction_id: response.nimbbl_transaction_id || "",
+              order_invoice:
+                (response.order && response.order.invoice_id) || "",
+              order_amount:
+                (response.transaction &&
+                  response.transaction.transaction_amount) ||
+                "",
+              transaction_id:
+                (response.transaction && response.transaction.transaction_id) ||
+                "",
+              order_status: response.status || "",
+              plan_type: plan_type || "",
+            };
+
+            // Example API request
+            const res = await axios.post(
+              `https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-payment`,
+              payload,
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${tokenParam}`,
+                },
+              }
+            );
+
+            console.log("Developer", res);
+          } catch (error) {
+            console.log("Error generate", error);
+          }
         };
 
         window.checkout = new NimbblCheckout(options);
@@ -113,6 +151,10 @@ const Pay = () => {
   };
 
   useEffect(() => {
+    if (planIdParam !== "default_order_id") {
+      fetchDataAndBuy();
+    }
+
     const script = document.createElement("script");
     script.src = "https://api.nimbbl.tech/static/assets/js/checkout.js";
     script.async = true;
@@ -121,29 +163,51 @@ const Pay = () => {
     return () => {
       document.body.removeChild(script);
     };
-  }, []);
-
-  useEffect(() => {
-    if (tokenParam !== "default_token" && planIdParam !== "default_order_id") {
-      fetchDataAndBuy();
-    }
-  }, []);
+  }, [planIdParam]);
 
   return (
-    <div className="App">
-      
-      <header className="App-header">
-        {/* creating a loading screen */}
-        {token === "" ? (
-          <div className="loading">
-            <h2>Loading...</h2>
+    <div style={styles.app}>
+      <h1 style={styles.heading}>Dorzet</h1>
+      <header style={styles.header}>
+        {loading ? (
+          // Loading screen
+
+          <div>
+            <p style={styles.loadingText}>Loading...</p>
+            <p style={styles.loadingDescription}>
+              Please wait while we are processing the payment
+            </p>
+          </div>
+        ) : null}
+
+        {orderStatus === "success" ? (
+          // Success screen
+          <div>
+            <Helmet>
+              <title>Payment Successful</title>
+            </Helmet>
+            <p style={styles.loadingText}>Payment Successful</p>
+            <p style={styles.loadingDescription}>
+              Your payment has been successfully processed
+            </p>
           </div>
         ) : (
-          <div className="App-link">
-            {/*loading screen  */}
-            {/*  */}
-            <p>loading </p>
-            <p>don`t refresh or move back</p>
+          <div>
+            <Helmet>
+              <title>Payment Failed</title>
+            </Helmet>
+            <p style={styles.loadingText}>Payment Failed</p>
+            {/*set text to make the payment again */}
+            <p style={styles.loadingDescription}>
+              Your payment has been failed
+            </p>
+            <button
+              onClick={() => {
+                fetchDataAndBuy();
+              }}
+            >
+              Make the payment again by clicking here
+            </button>
           </div>
         )}
       </header>
@@ -151,4 +215,32 @@ const Pay = () => {
   );
 };
 
-export default Pay;
+const styles = {
+  app: {
+    textAlign: "center",
+  },
+  heading: {
+    fontSize: "30px",
+    fontWeight: "bold",
+    color: "#000000",
+    marginBottom: "20px",
+  },
+  header: {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "black",
+  },
+  loadingText: {
+    fontSize: "30px",
+    fontWeight: "bold",
+  },
+  loadingDescription: {
+    fontSize: "20px",
+  },
+};
+
+export default UpgradePage;
--- src/pages/UpgradePage.jsx
+++ src/pages/UpgradePage.jsx
@@ -5,6 +5,7 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 
 const UpgradePage = () => {
   const [token, setToken] = useState("");
+  const [orderStatus, setOrderStatus] = useState("");
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const tokenParam = searchParams.get("token") || "default_token";
@@ -12,7 +13,7 @@ const UpgradePage = () => {
   const plan_type = searchParams.get("plan_type") || "one_time";
   const role = searchParams.get("role") || "default_role";
   const isupgrade = searchParams.get("isupgrade") || "false";
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const fetchDataAndBuy = async () => {
     try {
@@ -25,6 +26,7 @@ const UpgradePage = () => {
         return;
       }
 
+      // creating token for
       const response = await axios.post(
         "https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-token",
         {
@@ -66,46 +68,78 @@ const UpgradePage = () => {
         const options = {
           access_key: "access_key_81x7BagWlM1Rn05N",
           order_id: orderResponse.data.data.order_id,
-          callback_handler: function (response) {
-            console.log(response);
+          callback_handler: async function (response) {
+            // set order status
+            setOrderStatus(response.status);
+            if (response.status === "failed") {
+              // alert("Payment failed");
+
+              // Make the API request even for failed payments
+              await sendOrderAndCreatePayment(response);
+            }
+
             if (response.status === "success") {
               console.log(response.nimbbl_transaction_id);
               console.log(response.order_id);
               console.log(response.order.invoice_id);
               console.log(response.transaction.transaction_amount);
 
-              axios
-                .post(
-                  `https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-payment?role=${role}&isupgrade=true`,
-                  {
-                    nimbbl_order_id: response.order_id,
-                    nimbbl_transaction_id: response.nimbbl_transaction_id,
-                    order_invoice: response.order.invoice_id,
-                    order_amount: response.transaction.transaction_amount,
-                    transaction_id: response.transaction.transaction_id,
-                    order_status: response.status,
-                    plan_type: plan_type,
-                  },
-                  {
-                    headers: {
-                      "Content-Type": "application/json",
-                      Authorization: `Bearer ${tokenParam}`,
-                    },
-                  }
-                )
-                .then((res) => {
-                  console.log(res);
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
+              // Make the API request for successful payments
+              await sendOrderAndCreatePayment(response);
 
-              // change the url to the success page
-              navigate("/pay/success");
-            } else if (response.status === "failure") {
-              alert("Payment failed");
+              // Additional logic for successful payments
             }
           },
+        };
+
+        // Function to send order status and create detailed payment
+        const sendOrderAndCreatePayment = async (response) => {
+          try {
+            // Your logic to update order status
+
+            // Call the createPayment function
+            await createPayment(response);
+          } catch (error) {
+            console.error("Error:", error.message);
+          }
+        };
+
+        // Function to create detailed payment
+        // Function to create detailed payment
+        const createPayment = async (response) => {
+          try {
+            let payload = {
+              nimbbl_order_id: response.order_id || "",
+              nimbbl_transaction_id: response.nimbbl_transaction_id || "",
+              order_invoice:
+                (response.order && response.order.invoice_id) || "",
+              order_amount:
+                (response.transaction &&
+                  response.transaction.transaction_amount) ||
+                "",
+              transaction_id:
+                (response.transaction && response.transaction.transaction_id) ||
+                "",
+              order_status: response.status || "",
+              plan_type: plan_type || "",
+            };
+
+            // Example API request
+            const res = await axios.post(
+              `https://clumsy-puce-abalone.cyclic.app/api/v1/payment/create-payment?role=${role}&isupgrade=true`,
+              payload,
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${tokenParam}`,
+                },
+              }
+            );
+
+            console.log("Developer", res);
+          } catch (error) {
+            console.log("Error generate", error);
+          }
         };
 
         window.checkout = new NimbblCheckout(options);
@@ -132,24 +166,87 @@ const UpgradePage = () => {
   }, [planIdParam]);
 
   return (
-    <div className="App">
-     
-      <header className="App-header">
-        {/* creating a loading screen */}
+    <div style={styles.app}>
+      <h1 style={styles.heading}>Dorzet</h1>
+      <header style={styles.header}>
         {loading ? (
-          <div className="loading">
-            <h2>Loading...</h2>
+          // Loading screen
+
+          <div>
+            <p style={styles.loadingText}>Loading...</p>
+            <p style={styles.loadingDescription}>
+              Please wait while we are processing the payment
+            </p>
           </div>
+        ) : null}
+
+        {orderStatus === "success" ? (
+          // Success screen
+          <div>
+            <Helmet>
+              <title>Payment Successful</title>
+            </Helmet>
+            <p style={styles.loadingText}>Payment Successful</p>
+            <p style={styles.loadingDescription}>
+              Your payment has been successfully processed
+            </p>
+
+           
+          </div>
+
+
         ) : (
-          <div className="App-link">
-            {/* loading screen */}
-            <p>Loading </p>
-            <p>Don't refresh or move back</p>
+          <div>
+            <Helmet>
+              <title>Payment Failed</title>
+            </Helmet>
+            <p style={styles.loadingText}>Payment Failed</p>
+              {/*set text to make the payment again */}
+            <p style={styles.loadingDescription}>
+              Your payment has been failed
+            </p>
+            <button onClick={
+              () => {
+
+                fetchDataAndBuy();
+              
+              }
+            }>
+              Make the payment again by clicking here
+            </button>
           </div>
         )}
       </header>
     </div>
   );
+};
+
+const styles = {
+  app: {
+    textAlign: "center",
+  },
+  heading: {
+    fontSize: "30px",
+    fontWeight: "bold",
+    color: "#000000",
+    marginBottom: "20px",
+  },
+  header: {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "black",
+  },
+  loadingText: {
+    fontSize: "30px",
+    fontWeight: "bold",
+  },
+  loadingDescription: {
+    fontSize: "20px",
+  },
 };
 
 export default UpgradePage;
